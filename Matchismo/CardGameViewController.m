9d627ba9ca2dafad6b53ae63e3390863a60d8f2a
Ilya Shaisultanov <ishaisultanov@shutterstock.com>
2014-04-29 23:36:16 -0700

    Assignment 1 completed.

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -7,23 +7,61 @@
 //
 
 #import "CardGameViewController.h"
+#import "models/PlayingCardDeck.h"
+#import "models/Card.h"
 
 @interface CardGameViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
+@property (nonatomic) NSInteger flipsCount;
+@property (nonatomic) PlayingCardDeck *deck;
+@property (nonatomic, strong) Card *currentCard;
 
 @end
 
 @implementation CardGameViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+- (Deck *) deck {
+    if (!_deck) {
+        _deck = [[PlayingCardDeck alloc] init];
+    }
+    
+    return _deck;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void) setFlipsCount:(NSInteger)flipsCount {
+    _flipsCount = flipsCount;
+    
+    self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %ld", (long)self.flipsCount];
+}
+
+- (IBAction)touchCardButton:(UIButton *)sender {
+    UIImage *cardback = [UIImage imageNamed: @"cardback"];
+    UIImage *cardfront = [UIImage imageNamed: @"cardfront"];
+    
+    if ([sender.currentTitle length]) {
+        if (self.currentCard) {
+            [self.deck addCard:self.currentCard];
+        }
+        
+        self.currentCard = nil;
+        
+        [self setButton:sender toTitle:@"" withBackground:cardback];
+    } else {
+        Card *card = [self.deck drawRandomCard];
+        
+        self.currentCard = card;
+        
+        [self setButton:sender toTitle:[card contents] withBackground:cardfront];
+    }
+    
+    self.flipsCount++;
+}
+
+- (void) setButton:(UIButton *)sender toTitle:(NSString *)title withBackground:(UIImage *)image {
+    [sender setTitle:title forState:UIControlStateNormal];
+    [sender setBackgroundImage:image
+                      forState:UIControlStateNormal];
 }
 
 @end
